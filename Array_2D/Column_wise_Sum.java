13b085c29ad4d5e16d7a2d6a2dc2fc1aa7370311
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2025-05-12 18:14:32 +0530

    2D Array

---
+++ Array_2D/Column_wise_Sum.java
@@ -0,0 +1,18 @@
+package Array_2D;
+
+public class Column_wise_Sum {
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+        for (int j = 0; j < arr[0].length; j++) {
+            int colSum = 0;
+            for (int i = 0; i < arr.length; i++) {
+                colSum += arr[i][j];
+            }
+            System.out.println("Sum of Column " + j + ": " + colSum);
+        }
+    }
+}
---
+++ Array_2D/Row_wise_Sum.java
@@ -0,0 +1,18 @@
+package Array_2D;
+
+public class Row_wise_Sum {
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+        for (int i = 0; i < arr.length; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < arr[i].length; j++) {
+                rowSum += arr[i][j];
+            }
+            System.out.println("Sum of Row " + i + ": " + rowSum);
+        }
+    }
+}
