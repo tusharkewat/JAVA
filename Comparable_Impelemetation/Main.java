4f998f71ba78755fd3be6bcf9c6a5a721dd72bdd
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2025-06-05 21:54:06 +0530

    Comparable

---
+++ Comparable_Impelemetation/Main.java
@@ -0,0 +1,40 @@
+package Comparable_Impelemetation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Student implements Comparable<Student> {
+
+    int age;
+    String name;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public String toString() {
+        return age + " " + name;
+    }
+
+    public int compareTo(Student s) {
+        return this.age - s.age;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Student> list = new ArrayList<>();
+        list.add(new Student(20, "Tushar"));
+        list.add(new Student(17, "rahul"));
+        list.add(new Student(16, "rohit"));
+        list.add(new Student(11, "mohit"));
+        list.add(new Student(51, "om"));
+
+        Collections.sort(list);
+
+        for (Student s : list) {
+            System.out.println(s.toString());
+        }
+    }
+}
